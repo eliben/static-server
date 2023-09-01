bfc8a1244e94f84c48625f0798f8ea4a0e9f5b52
Eli Bendersky <eliben@gmail.com>
2023-08-31 17:37:14 -0700

    Custom Usage

--- main.go
+++ main.go
@@ -6,18 +6,29 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"log"
 	"net/http"
+	"os"
 )
 
+func usage() {
+	out := flag.CommandLine.Output()
+	fmt.Fprintf(out, "Usage: %v [dir]\n", os.Args[0])
+	fmt.Fprint(out, "\n  [dir] is optional; if not passed, '.' is used\n\n")
+	flag.PrintDefaults()
+}
+
 func main() {
+	flag.Usage = usage
 	hostFlag := flag.String("host", "", "specific host to listen on")
 	portFlag := flag.String("port", "8080", "port to listen on")
 	flag.Parse()
 
 	if len(flag.Args()) > 1 {
-		// TODO: print usage
-		log.Fatal("Error: too many command-line arguments")
+		log.Println("Error: too many command-line arguments")
+		usage()
+		os.Exit(1)
 	}
 
 	rootDir := "."
